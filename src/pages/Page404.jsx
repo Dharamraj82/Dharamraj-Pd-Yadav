1e9321b8886dd30935b637966d7ea811da4f7f9e
Dharamraj <dpy9572@gmail.com>
2025-10-12 20:21:01 +0530

    fixed page 404 page

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import FuzzyText from "../uiComponents/FuzzyText";
 
 function Page404() {
   return (
-    <div className="min-h-[80vh] flex justify-center items-center">
-      <FuzzyText
-        baseIntensity={0.2}
-        hoverIntensity={0.5}
-        enableHover={true}
-      >
+    <div className="min-h-[80vh] flex flex-col gap-5 justify-center items-center">
+   
+     <h4 className="md:text-6xl text-3xl text-red-500 font-bold">
         404 Page Not Found
-      </FuzzyText>
+     </h4>
+     <p>This page may be never exits</p>
     </div>
   );
 }
